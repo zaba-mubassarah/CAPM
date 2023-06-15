1c48ec8cee74e959d626eb1445e36117c3c754f5
zaba <zaba.mubassarah@gmail.com>
2023-06-15 11:56:28 +0600

    updated with unit test

---
+++ test/api.test.js
@@ -0,0 +1,55 @@
+const project = '../CAPM' // The project's root folder
+const cds = require('@sap/cds/lib')
+cds.test(project)
+
+const { GET, expect } = cds.test(project)
+describe('my test suite',  ()=>{
+
+ 
+  it('Create an entity', async () => {
+    const db = await cds.connect.to('db')  
+    const {Students} = db.model.entities('myCompany.hr.lms') 
+    await db.create(Students).entries([ 
+      {email: "shanita is a good girl", first_name: "demo", last_name: "demo", date_sign_up: "2019-10-10" }, 
+    ])
+    const { data } = await GET `/my-srv-demo/getStudents`
+    expect(data.value).to.containSubset([{email:"shanita is a good girl", first_name: "demo", last_name: "demo", date_sign_up: "2019-10-10" }])
+  })
+
+  it('Get an entity', async () => {
+    const { data } = await GET `/my-srv-demo/getStudents`
+    expect(data.value).to.containSubset([{email:"shanita is a good girl", first_name: "demo", last_name: "demo", date_sign_up: "2019-10-10" }])
+  })
+
+  it('Search an entity', async () => {
+    const { data } = await GET ('/my-srv-demo/getStudents', {
+          params: { $search: 'shanita is a good girl', $select: `email` }
+        })
+    expect(data.value).to.containSubset([{email:"shanita is a good girl", first_name: "demo", last_name: "demo", date_sign_up: "2019-10-10" }])
+  })
+  it('Update an entity', async () => {
+    const db = await cds.connect.to('db');
+    const { Students } = db.model.entities('myCompany.hr.lms');
+    
+    // Update the entity
+    await db.update(Students).set({ first_name: 'updated_demo' }).where({ email: 'shanita is a good girl' });
+  
+    // Verify the entity is updated
+    const { data } = await GET `/my-srv-demo/getStudents`;
+    expect(data.value).to.containSubset([{ email: 'shanita is a good girl', first_name: 'updated_demo' }]);
+  });
+  it('Delete an entity', async () => {
+    const db = await cds.connect.to('db');
+    const { Students } = db.model.entities('myCompany.hr.lms');
+    
+    // Delete the entity
+    await db.delete(Students).where({ email: 'shanita is a good girl' });
+  
+    // Verify the entity is deleted
+    const { data } = await GET `/my-srv-demo/getStudents`;
+    expect(data.value).to.not.containSubset([{ email: 'shanita is a good girl' }]);
+  });
+
+  
+ 
+})
